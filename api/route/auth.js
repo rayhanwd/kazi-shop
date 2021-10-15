c3b2a06f04e4ff1258155d225d16ce1f531fc95e
rayhanwd <rayhanbd4400@gmail.com>
2021-10-15 11:16:51 +0600

    removed confilct file

--- api/route/auth.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const authcontroller = require('../controller/auth');
-
-router.post('/register',authcontroller.registerAuth_control);
-router.get('/login',authcontroller.loginAuth_control);
-router.put('/:id',authcontroller.updateuser_control);
-router.delete('/:id',authcontroller.deleteuser_control);
-
-module.exports = router;
